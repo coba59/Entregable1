df713b2f04260efaf9ceb5d57f743d7a0bb11b1a
coba59 <coba.esr@gmail.com>
2022-06-03 20:27:07 -0300

    cambio3
    cambio3

--- script.js
+++ script.js
@@ -7,43 +7,50 @@
 // - repetir la salida del mensaje "HOLA" la cantidad de veces ingresada
 
 
-
-function sumarNumero() {
-    let numero1 = parseInt(prompt("Ingrese un número"));
-    let numero2 = parseInt(prompt("Ingrese el número que sera sumado en cada iteraccion"));
-    let suma = 0;
-        for (let i=0; i < 10; i++){
-            suma = numero1 + numero2;
+function sumarNumero() 
+    {
+        let numero1 = parseInt(prompt("Ingrese un número entre 1 y 9"));
+        let numero2 = parseInt(prompt("Ingrese el número que sera sumado en cada iteraccion"));
+        let suma = 0;
+            for (let i=0; i < 10; i++)
+            {
+                suma = numero1 + numero2;
+                console.log("Nro de iteracion: "+i)
+            }
             console.log("Suma: " +suma)
-            console.log("Nro de iteracion: "+i)
-            alert("Suma: " +suma)
-            alert("ALERT SUMAR NUMERO")
-        }
+            alert(numero1 + " + " + numero2 + " =  " + suma)          
     }
     
     sumarNumero()
     
-    function concatenarTexto() {
-    let texto = prompt("Ingrese un texto")
-    let result = texto
-    let txtConcat = prompt("Ingrese un texto para concatenar al texto anterior. Puede dejar de concatenar escribiendo ESC")
-    
-        while (txtConcat != 'ESC'){
-            result = result + " " + txtConcat
-            console.log(result)
-            alert(result)
-            alert("ALERT SUMAR RESULTADO")
-        }
+ function concatenarTexto() 
+    {
+        let texto = prompt("Vamos a concatenar dos textos. Ingrese el primer texto que servirá como base")
+        let result = texto
+        let txtConcat
+        
+            while (txtConcat != 'ESC')
+            {
+                let segundoTexto = prompt("Ingrese un texto para concatenar al texto anterior. Puede dejar de concatenar escribiendo ESC")
+                txtConcat = segundoTexto
+                result = result + " " + segundoTexto
+                console.log(result)
+                alert("Resultado de la concatenacion: " +result)
+            }
     }
     
     concatenarTexto()
-    
-    function repetirMensaje() {
-    let numero1 = parseInt(prompt("Ingrese un numero para repetir el mensaje"))
-    
-        for(let i = 1; i <= numero1; i++){
-            alert("Hola")
-        }
+
+
+    function repetirMensaje() 
+    {
+        alert("Vamos a repetir la palabra HOLA, la cantidad de veces ingresada.")
+        let numero1 = parseInt(prompt("Ingrese la cantidad de veces que quiere repetir la palabra"))
+        
+            for(let i = 1; i <= numero1; i++)
+            {
+                alert("HOLA (x" + i + ")")
+            }
     }
     
-    repetirMensaje() 
+    repetirMensaje()
